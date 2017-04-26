1073b95175f28b8d6d90df52a86bf716a9eb77f0
doraqiqi <598426456@qq.com>
2017-04-26 22:57:35 +0800

    优化购物车

--- shoppingCart_sell.py
+++ shoppingCart_sell.py
@@ -4,13 +4,13 @@ list = open("product","r",encoding="utf-8")
 
 product_list = []
 
-
+for line in list:
+    product_line = line.strip().split()
+#    product_line[1] = int(product_line[1])
+    product_line = tuple(product_line)
+    product_list.append(product_line)
 while True:
-    for line in list:
-        product_line = line.strip().split()
-        product_line[1] = int(product_line[1])
-        product_line = tuple(product_line)
-        product_list.append(product_line)
+
     for index, i in enumerate(product_list):
         print(index, i)
     print("-------------------------------")
@@ -21,12 +21,11 @@ while True:
     if choice == "1":
         input_product = input("请输入商品名：")
         input_price = input("请输入价格：")
+        add = [input_product,input_price]
+        product_list.append(tuple(add))
         f_product = open("product","a",encoding="utf-8")
-        f_product.write(input_product)
-        f_product.write(" ")
-        f_product.write(input_price)
-        f_product.write("\n")
-        f_product.close()
+        f_product.write(input_product+" "+input_price+"\n")
+#        f_product.close()
         print("已成功添加")
     elif choice =="2":
         input_del = input("请输入要删除的商品编号")
@@ -34,12 +33,21 @@ while True:
             input_del = int(input_del)
             product_list.pop(input_del)
             list_new = open("product", "w", encoding="utf-8")
-            for line in product_list:
-                list_new.write(line[0] + " " + str(line[1]) + "\n")
+            # for line in product_list:
+            #     print(line)
+            #     list_new.write(line[0] + " " + line[1] + "\n")
+                #list_new.close()
+            # print(product_list)
+            # c = 0
+            # while c < len(product_list):
+            #     list_new.write(product_list[c][0] + " " +product_list[c][1]+"\n")
+            #     c = c+1
         else:
             print("请输入数字！")
-
     elif choice == "3":
+        list_new2 = open("product", "w", encoding="utf-8")
+        for line in product_list:
+            list_new2.write(line[0] + " " + line[1] + "\n")
         exit()
     else:
         print("请输入正确选项！")
